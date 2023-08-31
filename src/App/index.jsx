f11be9ebfc7ad42b109fbea243b93cd5ec7ae7aa
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2023-08-31 14:27:40 -0300

    create Header component

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,22 +1,14 @@
 import './styles.css'
 
 import { Header } from "../components/Header"
-import { Navbar } from '../components/Navbar'
-import { Hero } from '../components/HeroSection'
-import { About } from '../components/AboutSection'
-import { Projects } from '../components/ProjectSection'
-import { Certificates } from '../components/CertficateSection'
+
 
 export function App() {
 
   return (
     <div className='app'>
       <Header />
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Certificates />
+
     </div>
   )
 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import "./styles.css"
+
+export function Header() {
+    return (
+        <header className="header">
+            <a href="/" className="info-link">
+                <strong className="name">Rafael Vasconcelos</strong>
+                <span className="role">Front-end Developer</span>
+            </a>
+        </header>
+    )
+}
