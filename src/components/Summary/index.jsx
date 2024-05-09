083447f88f1b4165ee9ba75599beead451538e32
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2024-05-09 19:44:02 -0300

    remove data list and restructure component

--- src/components/Summary/index.jsx
+++ src/components/Summary/index.jsx
@@ -1,91 +1,9 @@
 import "./styles.css";
 
-import brazilFlag from "../../assets/brazil.png"
-import canadaFlag from "../../assets/canada.png"
-import spainFlag from "../../assets/spain.png"
-
-import { Briefcase, GraduationCap, AirplaneTilt } from "@phosphor-icons/react"
-
-import { SummaryList } from "../SummaryList";
-import { SummaryTitle } from "../SummaryTitle";
-
-const lists = {
-  experiences: [
-    {
-      icon: <AirplaneTilt className="icon" />,
-      textLabel: "Exchange Student in Canada",
-      textDescription: "NB International Student Program",
-    },
-    {
-      icon: <Briefcase className="icon" />,
-      textLabel: "Web Designer",
-      textDescription: "Atimo US",
-    },
-    {
-      icon: <Briefcase className="icon" />,
-      textLabel: "Programming Monitor",
-      textDescription: "Federal Institute of Pernambuco",
-    },
-  ],
-  courses: [
-    {
-      icon: <GraduationCap className="icon" />,
-      textLabel: "Front-end Programming Course",
-      textDescription: "DevQuest",
-    },
-    {
-      icon: <GraduationCap className="icon" />,
-      textLabel: "UX Design Fundamentals Course",
-      textDescription: "Google",
-    },
-    {
-      icon: <GraduationCap className="icon" />,
-      textLabel: "Bachelor of Technology in Software Development",
-      textDescription: "Tiradentes University",
-    },
-  ],
-  idioms: [
-    {
-      icon: <img className="icon" src={brazilFlag} alt="brazilian flag" />,
-      textLabel: "Portuguese",
-      textDescription: "Native",
-    },
-    {
-      icon: <img className="icon" src={canadaFlag} alt="canadian flag" />,
-      textLabel: "English",
-      textDescription: "Fluent",
-    },
-    {
-      icon: <img className="icon" src={spainFlag} alt="spanish flag" />,
-      textLabel: "Spanish",
-      textDescription: "Intermediate",
-    },
-  ],
-}
-
-export function Summary() {
+export function Summary({ children }) {
   return (
     <div className="summary">
-      <header className="summary-header">
-        <SummaryTitle titleName="experience" titleContent="Experiences" active/>
-        <SummaryTitle titleName="course" titleContent="Education" />
-        <SummaryTitle titleName="idiom" titleContent="Idioms" />
-      </header>
-      <div className="summary-content">
-        <SummaryList
-          active
-          listName="experience"
-          listContent={lists.experiences}
-        />
-        <SummaryList
-          listName="course"
-          listContent={lists.courses}
-        />
-        <SummaryList
-          listName="idiom"
-          listContent={lists.idioms}
-        />
-      </div>
+      {children}
     </div>
   );
 }
