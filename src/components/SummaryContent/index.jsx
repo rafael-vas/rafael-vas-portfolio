8607f4bd8518b493547b73b9fa606c93b2f14d53
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2024-05-09 19:45:22 -0300

    create summary-content component

---
+++ src/components/SummaryContent/index.jsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export function SummaryContent({ children }) {
+    return (
+        <header className="summary-content">
+            {children}
+        </header>
+    )
+}
